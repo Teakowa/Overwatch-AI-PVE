fcdbd370cf3e6f62b67bab9f0b30e7ea09da2394
Teakowa <git@teakowa.dev>
2026-03-05 19:58:16 +0800

    refactor(post-compile-hook): 修正尖刺墙生命值字符串展示

--- src/post-compile-hook.js
+++ src/post-compile-hook.js
@@ -1,4 +1,5 @@
 // 修正骇灾技能字符串展示
 content = content.replace(/尖刺护体资源恢复倍率/g, "尖刺护体资源恢复");
 content = content.replace(/尖刺护体资源消耗倍率/g, "尖刺护体资源消耗");
+content = content.replace(/尖刺墙生命值倍率/g, "尖刺墙生命值");
 content.toString();
