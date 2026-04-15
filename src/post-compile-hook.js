d8e5a2b9fe21659f0da970ce7149a5c996b60ec7
Teakowa <git@teakowa.dev>
2026-04-15 17:24:49 +0800

    fix(post-compile-hook): 修正尖刺技能字符串展示

--- src/post-compile-hook.js
+++ src/post-compile-hook.js
@@ -2,4 +2,4 @@
 content = content.replace(/尖刺护体资源恢复倍率/g, "尖刺护体资源恢复");
 content = content.replace(/尖刺护体资源消耗倍率/g, "尖刺护体资源消耗");
 content = content.replace(/尖刺墙生命值倍率/g, "尖刺墙生命值");
-content.toString();
+content;
